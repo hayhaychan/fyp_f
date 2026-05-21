05d68c5f787285ef07b1b79ee324a9c7d0141f9d
UltralyticsAssistant <web@ultralytics.com>
2026-05-21 08:29:44 +0000

    Auto-format by https://ultralytics.com/actions

--- testing_main.py
+++ testing_main.py
@@ -1,14 +1,12 @@
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
+
 from ultralytics import YOLO
-import cvzone
 
 model = YOLO("/Users/hayhay/Documents/ultralytics-main/runs/detect/train11/weights/best.pt")
 
 results = model.predict(source=0, show=True)
 
-print (results)
+print(results)
 
-if cv2.waitKey(1) == ord('q'):
+if cv2.waitKey(1) == ord("q"):
     cv2.destroyAllWindows()
